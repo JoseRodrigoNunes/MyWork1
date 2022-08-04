698cb20c749c978f68147d388294df375871dd96
JoseRodrigoNunes <zening15@gmail.com>
2022-08-04 17:49:56 -0300

    Quarto commit do primeiro trabalho do treinamento da dynacoop

---
+++ Trabalho1/AccountService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Trabalho1
+{
+    internal class AccountService
+    {
+
+        public static int SetCategory(string s)
+        {
+            int iCategory = Convert.ToInt32(s);
+            while (iCategory != 1 && iCategory != 2)
+            {
+                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico inteiro entre as opções disponíveis" +
+                    "\n1 - Cliente Preferencial | 2 - Padrão");
+                iCategory = Convert.ToInt32(Console.ReadLine());
+            }
+            return iCategory;
+        }
+
+        public static int SetNumberOfEmployees(string s)
+        {
+            string sNumberOfEmployees = s;
+            int iNumberOfEmployees;
+            while ((!int.TryParse(sNumberOfEmployees, out iNumberOfEmployees)))
+            {
+                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico inteiro");
+                sNumberOfEmployees = Console.ReadLine();
+            }
+
+            return iNumberOfEmployees;
+        }
+
+        public static decimal SetSaldoAtual(string s)
+        {
+            decimal dSaldo = Convert.ToDecimal(s);
+            while (dSaldo <= -100000000000 || 100000000000 <= dSaldo)
+            {
+                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico no intervalo entre " +
+                    "-100.000.000.000 e 100.000.000.000");
+                dSaldo = Convert.ToDecimal(Console.ReadLine());
+            }
+            return dSaldo;
+        }
+    }
+}
--- Trabalho1/CreateAccountService.cs
+++ Trabalho1/CreateAccountService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Trabalho1
 {
-    class CreateAccountService
+    class CreateAccountService : AccountService
     {
         public static Entity GetAccount()
         {
@@ -36,43 +36,5 @@ namespace Trabalho1
             account["primarycontactid"] = new EntityReference("contact", gContactId);
             return account;
         }
-
-        private static int SetCategory(string s)
-        {
-            int iCategory = Convert.ToInt32(s);
-            while(iCategory != 1 && iCategory != 2)
-            {
-                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico inteiro entre as opções disponíveis" +
-                    "\n1 - Cliente Preferencial | 2 - Padrão");
-                iCategory = Convert.ToInt32(Console.ReadLine());
-            }
-            return iCategory;
-        }
-
-        private static int SetNumberOfEmployees(string s)
-        {
-            string sNumberOfEmployees = s;
-            int iNumberOfEmployees;
-            while ((!int.TryParse(sNumberOfEmployees, out iNumberOfEmployees)))
-            {
-                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico inteiro");
-                sNumberOfEmployees = Console.ReadLine();
-            }
-
-            return iNumberOfEmployees;
-        }
-
-        private static decimal SetSaldoAtual(string s)
-        {
-            decimal dSaldo = Convert.ToDecimal(s);
-            while (dSaldo <= -100000000000 || 100000000000 <= dSaldo)
-            {
-                Console.WriteLine("\nPor favor certifique-se que está digitando um valor numérico no intervalo entre " +
-                    "-100.000.000.000 e 100.000.000.000");
-                dSaldo = Convert.ToDecimal(Console.ReadLine());
-            }
-            return dSaldo;
-        }
-        
     }
 }
